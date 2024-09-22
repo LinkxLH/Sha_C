a2ff2f0c3fe3c865c5c796345c9d005de676e13f
EverGreen <thekingthekongshalom@gmail.com>
2024-09-22 19:16:42 +0300

    חישוב מיקום

--- tankDrive/src/main/java/frc/robot/Robot.java
+++ tankDrive/src/main/java/frc/robot/Robot.java
@@ -4,6 +4,11 @@
 
 package frc.robot;
 
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -12,7 +17,6 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
-
   @Override
   public void robotInit() {
     m_robotContainer = new RobotContainer();
@@ -21,7 +25,7 @@ public class Robot extends TimedRobot {
   @Override
   public void robotPeriodic() {
     CommandScheduler.getInstance().run();
-  }
+    }
 
   @Override
   public void disabledInit() {}
--- tankDrive/src/main/java/frc/robot/subsystems/TankDrive.java
+++ tankDrive/src/main/java/frc/robot/subsystems/TankDrive.java
@@ -1,10 +1,16 @@
 package frc.robot.subsystems;
 
+import com.kauailabs.navx.frc.AHRS;
 import com.revrobotics.CANSparkBase;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkPIDController;
+import com.revrobotics.CANSparkBase.ControlType;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.SerialPort.Port;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.Utils.Vector2d;
 
 public class TankDrive extends SubsystemBase{
     
@@ -13,7 +19,10 @@ public class TankDrive extends SubsystemBase{
     private CANSparkMax m_motorLB;
     private CANSparkMax m_motorRT;
     private CANSparkMax m_motorRB;
-
+    private AHRS m_gyro;
+    private Vector2d m_currentPoint;
+    private double m_lastTimeMilis;
+    
     public TankDrive(){
        m_motorLB = new CANSparkMax(0, MotorType.kBrushless);
        m_motorRB = new CANSparkMax(0, MotorType.kBrushless);
@@ -21,6 +30,9 @@ public class TankDrive extends SubsystemBase{
        m_motorRT = new CANSparkMax(0, MotorType.kBrushless);
        m_motorLB.follow(m_motorLT);
        m_motorRB.follow(m_motorRT);
+       m_lastTimeMilis = System.currentTimeMillis();
+       m_gyro = new AHRS(Port.kMXP);
+       
     }
 
     public static TankDrive getInstance(){
@@ -28,6 +40,19 @@ public class TankDrive extends SubsystemBase{
             m_instance = new TankDrive();
 
         return m_instance;
+    }
+    
+    
+    @Override
+    public void periodic() {
+        double currentTimeMilis = System.currentTimeMillis();
+        double deltaTimeSec = (currentTimeMilis - m_lastTimeMilis) / 1000.0;
+        double leftCurrentSpeed = m_motorLT.getEncoder().getVelocity();
+        double rightCurrentSpeed = m_motorRT.getEncoder().getVelocity();
+        Vector2d velocityVector2d = new Vector2d(0,leftCurrentSpeed+rightCurrentSpeed);
+        velocityVector2d.rotate(Math.toRadians(m_gyro.getAngle()));
+        m_lastTimeMilis = currentTimeMilis;
+        m_currentPoint.add(velocityVector2d.mul(deltaTimeSec));
     }
 
     public void drive(double speedL, double speedR){  
